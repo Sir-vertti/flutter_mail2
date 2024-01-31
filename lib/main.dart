12263eb2187d5b4ce4115cfe5286aa14e0bdc5e2
Ingeniero_Verti <116362268+Sir-vertti@users.noreply.github.com>
2024-01-31 11:29:01 -0600

    hi

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-/// Flutter code sample for [ListTile].
-
 void main() => runApp(const ListTileApp());
 
 class ListTileApp extends StatelessWidget {
@@ -17,97 +15,255 @@ class ListTileApp extends StatelessWidget {
   }
 }
 
-class ListTileExample extends StatelessWidget {
+class ListTileExample extends StatefulWidget {
   const ListTileExample({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _ListTileExampleState createState() => _ListTileExampleState();
+}
+
+class _ListTileExampleState extends State<ListTileExample> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    const Page1(),
+    const Page3(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Mailboxes')),
+      appBar: AppBar(title: const Text('AppBebidas')),
       body: Stack(
         children: [
-          ListView(
-            children: const <Widget>[
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.cloud),
-                  title: Text('iCloud Drive'),
-                  trailing: Icon(Icons.arrow_forward_ios),
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.phone_iphone),
-                  title: Text('iPhone Drive'),
-                  trailing: Icon(Icons.arrow_forward_ios),
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.cloud_done),
-                  title: Text('pCloud Recently Deleted'),
-                  trailing: Icon(Icons.arrow_forward_ios),
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.favorite),
-                  title: Text('Favorites'),
-                  trailing: Icon(Icons.arrow_forward_ios),
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.download),
-                  title: Text('Download'),
-                  trailing: Icon(Icons.arrow_forward_ios),
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.tag),
-                  title: Text('Tags'),
-                  trailing: Icon(Icons.arrow_forward_ios),
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.work),
-                  title: Text('Work'),
-                  trailing: Icon(Icons.arrow_forward_ios),
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.home),
-                  title: Text('Home'),
-                  trailing: Icon(Icons.arrow_forward_ios),
-                ),
-              ),
-            ],
+          Positioned.fill(
+            child: _pages[_currentIndex],
           ),
           const Positioned(
-            top: 8.0, // Ajusta la distancia desde la parte superior
-            right: 16.0, // Ajusta la distancia desde la derecha
+            top: 8.0,
+            right: 16.0,
             child: Padding(
               padding: EdgeInsets.all(16.0),
               child: Text(
-                'Edit',
-                style: TextStyle(fontSize: 18),
+                'Utem',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.green,
+                ),
               ),
             ),
           ),
         ],
       ),
-      bottomNavigationBar: const BottomAppBar(
-        child: SizedBox(
-          height: 50,
-          child: Center(
-            child: Text(
-              'uploaded just now',
-              style: TextStyle(fontSize: 18),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.history),
+            label: 'Historial',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Page1 extends StatelessWidget {
+  const Page1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          buildListTile(context, 'Café', 'Receta de Café', Icons.local_cafe),
+          buildListTile(
+              context, 'Té', 'Receta de Té', Icons.emoji_food_beverage),
+          buildListTile(context, 'Agua', 'Receta de Agua', Icons.water),
+        ],
+      ),
+    );
+  }
+
+  Widget buildListTile(
+      BuildContext context, String title, String recipe, IconData icon) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 2,
+              blurRadius: 5,
+              offset: const Offset(0, 3),
+            ),
+          ],
+        ),
+        child: ListTile(
+          contentPadding: EdgeInsets.zero,
+          tileColor: Colors.transparent,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          leading: Icon(icon, color: Colors.teal),
+          title: Text(
+            title,
+            style: const TextStyle(
+              fontSize: 18.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.teal,
             ),
           ),
+          subtitle: Text(
+            recipe,
+            style: const TextStyle(
+              fontSize: 14.0,
+              color: Colors.grey,
+            ),
+          ),
+          trailing: const Icon(Icons.arrow_forward, color: Colors.teal),
+          onTap: () {
+            if (title == 'Café') {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const CafePage(),
+                ),
+              );
+            } else if (title == 'Té') {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const TePage(),
+                ),
+              );
+            } else if (title == 'Agua') {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const AguaPage(),
+                ),
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class Page3 extends StatelessWidget {
+  const Page3({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Historial',
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+  }
+}
+
+class CafePage extends StatelessWidget {
+  const CafePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Página de Café'),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Café',
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Receta de Café:\n\nIngredientes:\n- Café molido\n- Agua caliente\n\nInstrucciones:\n1. Agrega café molido al filtro de la cafetera.\n2. Vierte agua caliente sobre el café.\n3. Espera a que se filtre y disfruta.',
+              style: TextStyle(fontSize: 16),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TePage extends StatelessWidget {
+  const TePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Página de Té'),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Té',
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Receta de Té:\n\nIngredientes:\n- Bolsitas de té\n- Agua caliente\n\nInstrucciones:\n1. Coloca una bolsita de té en una taza.\n2. Vierte agua caliente sobre la bolsita de té.\n3. Deja reposar y disfruta.',
+              style: TextStyle(fontSize: 16),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AguaPage extends StatelessWidget {
+  const AguaPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Página de Agua'),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Hola desde la Página de Agua',
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Receta de Agua:\n\nIngredientes:\n- Agua\n\nInstrucciones:\n1. Toma un vaso.\n2. Vierte agua en el vaso.\n3. Disfruta de un vaso de agua refrescante.',
+              style: TextStyle(fontSize: 16),
+              textAlign: TextAlign.center,
+            ),
+          ],
         ),
       ),
     );
